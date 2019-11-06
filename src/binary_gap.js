0405862c6c687d2b8bc7203e0162c8612f412e00
Harlley Oliveira <harlley@gmail.com>
2019-11-06 12:30:52 +0000

    remove bug

--- src/binary_gap.js
+++ src/binary_gap.js
@@ -10,47 +10,27 @@ function solution(N) {
     return 0;
   }
 
-  let regex = new RegExp(/1?0+1/g);
-
-  if (regex.test(bin)) {
-    let gaps = [];
-    let startGap = false;
-    let endGap = false;
-    let sizeCurrentEventualGap = 0;
+  let gaps = [];
+  let startGap = false;
+  let endGap = false;
+  let sizeCurrentEventualGap = 0;
     
-    for (let bit of bin) {
-      if (bit === '1' && !startGap && !endGap) { // start counting
-        startGap = true;
-        continue;
-      }
-      if (bit === '1' && startGap) { // finish couting
-        endGap = true;
-        gaps.push(sizeCurrentEventualGap);
-      }
-      if (bit === '0' && startGap && !endGap) { // counting
-        sizeCurrentEventualGap = sizeCurrentEventualGap + 1;
-      }
+  for (let bit of bin) {
+    if (bit === '1' && !startGap && !endGap) { // start counting
+      startGap = true;
+      continue;
     }
-    return Math.max.apply(null, gaps);
+    if (bit === '1' && startGap) { // finish couting
+      gaps.push(sizeCurrentEventualGap);
+      sizeCurrentEventualGap = 0;
+    }
+    if (bit === '0' && startGap && !endGap) { // counting
+      sizeCurrentEventualGap = sizeCurrentEventualGap + 1;
+    }
   }
-  else {
-    return 0;
-  }
+
+  return Math.max.apply(null, gaps);
 
 }
 
 export default solution;
-
-    //console.log(bin.split("1"));
-    // const gapsLen = bin.split("1").map(val => val.length);
-    // return Math.max.apply(null, gapsLen);
-    //console.log(...bin);
-
-// console.log("solution() =  ~>", solution(1041));
-// console.log("solution() =  ~>", solution(32));
-// console.log("solution() =  ~>", solution(9));
-// console.log("solution() =  ~>", solution(20));
-// console.log("solution() =  ~>", solution(15));
-// console.log("solution() =  ~>", solution(6));
-// console.log("solution() =  ~>", solution(328));
-// console.log("solution() = 2 ~>", solution(51712));
